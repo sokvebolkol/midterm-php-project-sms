be05f5e5c467b238fcaeff45e927e367efe13621
sokvebolkol <sokvebol.kol@gmail.com>
2020-08-05 16:31:10 +0700

    add faker data

--- db/school.sql
+++ db/school.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100411
  File Encoding         : 65001
 
- Date: 19/07/2020 17:25:04
+ Date: 05/08/2020 16:30:13
 */
 
 SET NAMES utf8mb4;
@@ -31,7 +31,7 @@ CREATE TABLE `admin`  (
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES (1, 'admin', '12345');
+INSERT INTO `admin` VALUES (1, 'admin', '1234');
 
 -- ----------------------------
 -- Table structure for class
@@ -42,13 +42,14 @@ CREATE TABLE `class`  (
   `CNAME` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `CSEC` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`CID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of class
 -- ----------------------------
 INSERT INTO `class` VALUES (8, 'A', 'Morning');
 INSERT INTO `class` VALUES (9, 'B', 'Afternoon');
+INSERT INTO `class` VALUES (11, 'D', 'Morning');
 
 -- ----------------------------
 -- Table structure for student
@@ -68,14 +69,17 @@ CREATE TABLE `student`  (
   `SSEC` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `SIMG` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES (1, 'S001', 'sokvebol kol', 'Modric', '11-03-2005', 'Male', '0997476354', 'sokvebol.kol@gmail.com', '371 Phnom Penh', 'VIII', 'B', 'student/');
-INSERT INTO `student` VALUES (2, 'S001', 'sokvebol kol', 'Modric', '11-03-2005', 'Male', '0997476354', 'sokvebol.kol@gmail.com', '371 Phnom Penh', 'VIII', 'B', 'student/fewc8ez6r2e2agah717y.png');
-INSERT INTO `student` VALUES (3, 'S2', 'testing', 'Modric', '15-05-2006', 'Male', '0997476354', 'sokvebol.kol@gmail.com', '371 Phnom Penh', 'VIII', 'C', 'student/DFD-of-e-learning.PNG');
+INSERT INTO `student` VALUES (1, 'S001', 'sokvebol', 'kol', '5-11-2001', 'Male', '', 'sokvebol.kol@gmail.com', '#371 Phnom Penh', 'A', 'Morning', 'student/cristiano-ronaldo-warm.jpeg');
+INSERT INTO `student` VALUES (4, 'S2', 'seth', 'pho', '15-03-2006', 'Male', '', 'seth@gmail.com', '#Odor meanchey province,Cambodia', 'A', 'Morning', 'student/photo_2020-01-24_09-37-55.jpg');
+INSERT INTO `student` VALUES (5, 'S3', 'oudom', 'heang', '10-04-2007', 'Male', '0997476354', 'oudom@gmail.com', '#Kampong Cham Province,Cambodia', 'B', 'Afternoon', 'student/download.jfif');
+INSERT INTO `student` VALUES (6, 'S4', 'ronaldo', 'cristein', '13-03-2007', 'Male', '', 'ronaldo@gmail.com', 'Purtuygal ', 'B', 'Afternoon', 'student/cristiano-ronaldo-warm.jpeg');
+INSERT INTO `student` VALUES (7, 'S5', 'angela', 'noop', '7-03-2006', 'Female', '', 'angela@gmail.com', '#Mobile legend', 'D', 'Morning', 'student/download (1).jfif');
+INSERT INTO `student` VALUES (8, 'S6', 'bruno', 'fernades', '3-02-2016', 'Male', '', 'bruno@gmail.com', '#putoygal', 'B', 'Afternoon', 'student/images.jfif');
 
 -- ----------------------------
 -- Table structure for sub
@@ -85,7 +89,7 @@ CREATE TABLE `sub`  (
   `SID` int(11) NOT NULL AUTO_INCREMENT,
   `SNAME` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`SID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sub
@@ -96,5 +100,6 @@ INSERT INTO `sub` VALUES (5, 'Social Science');
 INSERT INTO `sub` VALUES (7, 'php');
 INSERT INTO `sub` VALUES (8, 'Database');
 INSERT INTO `sub` VALUES (9, 'Mobile Development');
+INSERT INTO `sub` VALUES (10, 'React native');
 
 SET FOREIGN_KEY_CHECKS = 1;
